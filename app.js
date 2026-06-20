cd5739305a57ba0bc5f722a748e3448aa1309568
mac070866 <micheleann.acosta78@gmail.com>
2026-06-19 22:55:35 -0400

    update new commit

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ console.log(''.length)
 console.log('12345'.length)
 let str = 'David'
 console.log(str[str.length - 1])
+console.log('123')
+
+let name = 'Michele'
+let fullName = 'Michele Acosta'
+
+let isRaining = true
+let temperature = 30
+let planet = 'Earth'
+
+temperature = temperature + 2
+
+console.log(temperature)
+
+
+/*
+*Converting Temperatures
+*
+*Celsius-to-Fahrenheit formula:
+*
+*   F = C x 1.8 + 32
+*/
+
+let celsius = 10;
+let fahrenheit = celsius * 1.8 + 32;
+
+
+let bool = !true
+
+console.log(bool)
