fe8e4469be0ef52d62b5d5c1a91da2af598afbba
mac070866 <micheleann.acosta78@gmail.com>
2026-06-22 21:18:29 -0400

    update new commit

--- app.js
+++ app.js
@@ -37,3 +37,34 @@ let fahrenheit = celsius * 1.8 + 32;
 let bool = !true
 
 console.log(bool)
+
+
+
+let subscribed = false
+let loggedIn = true
+
+if (subscribed === true) {
+    console.log ('show the video')
+}
+else if (loggedIn === true) {
+    console.log('tell the user to upgrade their subscription')
+}
+else {
+    console.log('tell user to log into account')
+}
+
+
+
+let cash = 30
+let price = 40
+
+if (cash > price) {
+    console.log("you paid extra - here's your change")
+}
+else if (cash === price) {
+    console.log("you paid the exact amount, have a nice day!")
+}
+else {
+    console.log("not enough money - you still owe x dollars")
+}
+
