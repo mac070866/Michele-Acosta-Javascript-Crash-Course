fd28bb3417f58d109b4608bbb0159156f20a574d
mac070866 <micheleann.acosta78@gmail.com>
2026-07-07 20:19:44 -0400

    update new commit

--- app.js
+++ app.js
@@ -266,3 +266,12 @@ const convertCelsiusToFahrenheit = (celsius) => {
 }
 
 console.log(convertCelsiusToFahrenheit(0))
+
+
+let item1 = 20
+let item2 = 30
+let item3 = 40
+let item4 = 50
+let item5 = 100
+
+let arr = [20. 30. 40, 50, 100]
