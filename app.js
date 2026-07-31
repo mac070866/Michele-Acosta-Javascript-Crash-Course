e5b238fa46602188a812c6bc28e7f0220e61e1a6
mac070866 <micheleann.acosta78@gmail.com>
2026-07-30 21:46:37 -0400

    update new commit

--- app.js
+++ app.js
@@ -402,18 +402,20 @@ let users = [
     discordId:  'David Bragg#0001',
     lessonsCompleted: [0, 1],
 },
-{   username:  'Mitri',
-    email: 'mitri@frontendsimplied.com',
-    password: 'mitri123',
-    subcriptionStatus:  'VIP',
-    discordId:  'Mitri#0001',
-    lessonsCompleted: [0, 1, 2, 3]
+{
+},
+{   
 },
 ];
-
-
 function login(email, password) {
-console.log(email, password);
+for (let i = 0; i < users.length; ++i) {
+    if (users[i].email === email) {
+    console.log(users[i]);
+    if (users[i].password === password) {
+        console.log('log the user in - the details are correct')
+    }
+}
+}
 }
 
 login('david@frontendsimplified.com', 'test123');
